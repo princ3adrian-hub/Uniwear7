ce6ce15d57063fd0da47223d5606839756182060
micsdev <micogene.pregoner01@gmail.com>
2026-09-11 14:02:21 +0800

    MAGCALING OUTPUT

--- app/src/test/java/quarter2/Magcaling_MiniPeta1.java
+++ app/src/test/java/quarter2/Magcaling_MiniPeta1.java
@@ -1,4 +1,20 @@
 package quarter2;
 
+import org.junit.Test;
+
 public class Magcaling_MiniPeta1 {
-}
+    @Test
+    public void printMYprofile () {
+        //   --- 1. THE INPUT (Storing your personal details in variables) --
+        String myName = "Mharc";
+        String petName = "Choco";
+        String favFood = "Adobo";
+        int myAge = 17;
+
+        // --- 2. THE OUTPUT (Printing to the console) ---
+        System.out.println("--- MY DIGITAL PROFILE ---");
+        System.out.println("Hello, my name is " + myName + " and i am  " + myAge + " years old. ");
+        System.out.println("I have a wonderful pet named " + petName + ".");
+        System.out.println("If i could, I would eat " + favFood + " every single day!");
+
+    }    }
