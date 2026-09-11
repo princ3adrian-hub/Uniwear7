0d158ea6603d5962aa85d71243380286e50c224b
prince <princ3adrian@gmail.com>
2026-09-11 14:00:26 +0800

    PAGURIGAN OUTPUT

--- app/src/test/java/quarter2/Paguirigan_MiniPeta1.java
+++ app/src/test/java/quarter2/Paguirigan_MiniPeta1.java
@@ -1,4 +1,23 @@
 package quarter2;
 
+import org.junit.Test;
+
 public class Paguirigan_MiniPeta1 {
+    @Test
+    public void PrintMyProfile() {
+        // --- The INPUT (Storing your personal details in variables) ---
+        String myName = "Prince";
+        String School = "Manila Cathedral School";
+        String petName = "Goya";
+        int myAge = 16;
+
+        // --- 2. THE OUTPUT (Printing to the console) ---
+        System.out.println("--- MY DIGITAL PROFILE! ---");
+        System.out.println("Hello! My name is " + myName + " and i am " + myAge + " years old");
+        System.out.println("I have a wonderful cat named " + petName + " . ");
+        System.out.println("And I am a student at the school " + School + ".");
+
+
+    }
 }
+
