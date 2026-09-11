1cf682b5252184d160e215e1445831d27b8241f6
SHS PC <mgermanesgilbert@gmail.com>
2026-09-11 13:56:55 +0800

    GERMANES OUTPUT

--- app/src/test/java/quarter2/Germanes_MiniPeta1.java
+++ app/src/test/java/quarter2/Germanes_MiniPeta1.java
@@ -1,4 +1,20 @@
 package quarter2;
 
+import org.junit.Test;
+
 public class Germanes_MiniPeta1 {
+    @Test
+    public void printMyProfile() {
+        //---I. THE INPUT (Storing your personal details in variable) ---
+        String myName = "Gilbert";
+        String favTeacher = "Sir Sixson";
+        String favClassmate = "Prince";
+        int myAge = 16;
+
+        //---2. THE OUTPUT(Printing to the console)---
+        System.out.println("--- MY DIGITAL PROFILE--");
+        System.out.println("Hello my name is " + myName + "myfavorite teacher is" + favTeacher + "!");
+        System.out.println("My Favorite Classmate from my class is" + favClassmate + "!");
+
+    }
 }
