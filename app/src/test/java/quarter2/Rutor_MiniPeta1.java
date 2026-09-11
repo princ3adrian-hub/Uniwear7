7fb3f06801d666c70cdeba2e1034892fbec460b7
RUTOR <rutorjanzzen@gmail.com>
2026-09-11 14:05:24 +0800

    RUTOR OUTPUT

--- app/src/test/java/quarter2/Rutor_MiniPeta1.java
+++ app/src/test/java/quarter2/Rutor_MiniPeta1.java
@@ -1,4 +1,20 @@
 package quarter2;
 
+import org.junit.Test;
+
 public class Rutor_MiniPeta1 {
+    @Test
+    public void printmyprofile() {
+        // --- 1.  THE INPUT (Storing your personal details in variables) --
+        String myName = "Gerard";
+        String petName = "Otep";
+        String favFood = "Mcdo";
+        int myAge = 16;
+
+// --- 2. THE OUTPUT (Printing to the console) ---
+        System.out.println("--- MY DIGITAL PROFILE ---");
+        System.out.println("Hello, my name is " + myName + " and I am " + myAge + " years old.");
+        System.out.println("I have a wonderful pet named" + petName + ".");
+        System.out.println("If I could, I would eat " + favFood + "every single day!");
+    }
 }
